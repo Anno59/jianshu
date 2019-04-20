9520c127f42cccfc0c04bcb295d68c266f2e1844
Anno59 <372025284@qq.com>
2019-04-20 20:35:22 +0800

    add react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import AppItem from './App.js';
+import { GlobalStyle } from './style'
+// import { GlobalIconStyle } from './statics/iconfont/iconfont';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const App = (
+  <div>
+    <GlobalStyle/>
+    <AppItem/>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
