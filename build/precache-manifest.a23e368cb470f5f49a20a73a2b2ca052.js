a50d8a7446613cec8df18e7a5baa289016153970
anno59 <yubl@c-bim.net>
2019-06-18 11:28:18 +0800

    add preview page

---
+++ build/precache-manifest.a23e368cb470f5f49a20a73a2b2ca052.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34cb37fbdea1cc137997",
+    "url": "./static/js/runtime~main.6feb3926.js"
+  },
+  {
+    "revision": "bb02e6381867a00fa053",
+    "url": "./static/js/main.489b664f.chunk.js"
+  },
+  {
+    "revision": "137c4a49ed8995986ab7",
+    "url": "./static/js/3.f27dd05f.chunk.js"
+  },
+  {
+    "revision": "5fd02ac7fd6c84662b43",
+    "url": "./static/js/2.900d13be.chunk.js"
+  },
+  {
+    "revision": "ec62c8ec65778769665c8a1ec22778c3",
+    "url": "./index.html"
+  }
+];
